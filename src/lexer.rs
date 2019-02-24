ad82b07725fd64891fbbd056602161a579adae97
Matthieu Bresson <matthieu.bresson@outlook.com>
2019-02-25 00:51:57 +0800

    rewrote and completed the lexer, removed Token's From<&str> impl
    Rewrote Lexer to make it easier to support symbols longer than one
    character (e.g. '==').
    Added support for remaining tokens to the lexer.
    Removed Token's From<&str> impl because this logic depends upon the
    context (previous and next characters, e.g. to check if the next
    character is a space), so it belongs more to Lexer than to the token
    module.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,284 @@
-use crate::token::Token;
+use crate::token::{keywords, Literal, Token};
+
+/// A Lexer iterates over a text and returns tokens.
+pub struct Lexer<'a> {
+    chars_iterator: std::str::Chars<'a>,
+    previous_char: Option<char>,
+}
+
+impl<'a> Lexer<'a> {
+    /// # Examples
+    ///
+    /// ```
+    /// let source_code = "
+    /// 1 + 1
+    /// ";
+    ///
+    /// let mut lexer = Lexer::new(source_code);
+    /// ```
+    pub fn new(text: &'a str) -> Self {
+        let mut chars_iterator = text.chars();
+        let first_char = chars_iterator.next();
+
+        Lexer {
+            chars_iterator,
+            previous_char: first_char,
+        }
+    }
+
+    fn get_next_non_whitespace_char_or_eof(&mut self) -> Option<char> {
+        while let Some(current_char) = self.chars_iterator.next() {
+            if !current_char.is_whitespace() {
+                return Some(current_char);
+            }
+        }
+
+        None
+    }
+
+    // returns None if the literal doesn't correspond to a known language keyword
+    fn literal_to_keyword(literal: &str) -> Option<Token> {
+        match literal {
+            keywords::FUNCTION => Some(Token::Function),
+            keywords::LET => Some(Token::Let),
+            keywords::TRUE => Some(Token::True),
+            keywords::FALSE => Some(Token::False),
+            keywords::IF => Some(Token::If),
+            keywords::NOT => Some(Token::Not),
+            keywords::RETURN => Some(Token::Return),
+            _ => None,
+        }
+    }
+
+    fn parse_identifier_or_keyword(literal: &str) -> Token {
+        match Self::literal_to_keyword(literal) {
+            Some(keyword) => keyword,
+            None => Token::Identifier(Literal(literal.to_string())),
+        }
+    }
+
+    // identifiers and keywords are sequences or letters, digits and/or underscores, and must start with a letter
+    //
+    // e.g.
+    // 'abcd' is an identifier
+    // 'a_42_' is an identifier
+    // 'fungsi' is a keyword (because it is one of the defined language keywords)
+    //
+    // this function assumes that `first_letter` is indeed a letter
+    fn read_identifier_or_keyword(&mut self, first_letter: char) -> Token {
+        let mut token_buffer = first_letter.to_string();
+
+        // read following characters until the first char that is not a letter, digit or underscore
+        while let Some(current_char) = self.chars_iterator.next() {
+            if current_char.is_alphanumeric() || current_char == '_' {
+                token_buffer.push(current_char);
+            } else {
+                self.previous_char = Some(current_char);
+
+                return Self::parse_identifier_or_keyword(&token_buffer);
+            }
+        }
+
+        // or until no more char can be read (EOF)
+        self.previous_char = None;
+
+        Self::parse_identifier_or_keyword(&token_buffer)
+    }
+
+    fn parse_base_10_integer(literal: &str) -> Token {
+        match literal.parse::<i64>() {
+            Ok(integer) => Token::Integer(integer),
+            Err(parsing_error) => {
+                // this should never happen, since at this stage, token_buffer should only contain digits,
+                // however:
+                // if the literal represents a number too big to fit in the i64 type, we will get an error
+                // in a production lexer we should return a meaningful error type, but this is just a toy project,
+                // so we'll be content with returning Token::Illegal and printing the real error to stderr
+                eprintln!(
+                    "Error while trying to parse sequence of digits '{}' to integer: {}",
+                    literal, parsing_error
+                );
+                Token::Illegal(Literal(literal.to_string()))
+            }
+        }
+    }
+
+    // integers are sequences of digits
+    //
+    // e.g
+    // '42' is an integer
+    // '09' is an integer
+    // '13.4' is not a valid integer
+    //
+    // this function assumes that `first_digit`  is indeed a digit
+    fn read_integer(&mut self, first_digit: char) -> Token {
+        let mut token_buffer = first_digit.to_string();
+
+        let number_base = 10;
+
+        // read following characters until the first non-digit
+        while let Some(current_char) = self.chars_iterator.next() {
+            if current_char.is_digit(number_base) {
+                token_buffer.push(current_char);
+            } else {
+                self.previous_char = Some(current_char);
+
+                return Self::parse_base_10_integer(&token_buffer);
+            }
+        }
+
+        // or until no more char can be read (EOF)
+        self.previous_char = None;
+
+        Self::parse_base_10_integer(&token_buffer)
+    }
+
+    // symbols are predefined sequences of one or more characters that are neither letters, nor digits or whitespaces
+    //
+    // e.g.
+    // '{' is the opening brace symbol
+    // '==' is the equality check symbol and is an operator
+    // '_' is not a recognized symbol, so it is invalid
+    fn read_special_symbol(&mut self, first_char: char) -> Token {
+        match first_char {
+            '(' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::OpeningParenthesis
+            }
+            ')' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::ClosingParenthesis
+            }
+            '{' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::OpeningBrace
+            }
+            '}' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::ClosingBrace
+            }
+            ',' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::Comma
+            }
+            ';' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::Semicolon
+            }
+            '+' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::Plus
+            }
+            '-' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::Minus
+            }
+            '*' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::Asterisk
+            }
+            '/' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::Slash
+            }
+            '<' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::LessThan
+            }
+            '>' => {
+                self.previous_char = self.chars_iterator.next();
+
+                Token::GreaterThan
+            }
+            '=' => {
+                // can be either Assign ("=") or Equal ("==")
+
+                match self.chars_iterator.next() {
+                    Some('=') => {
+                        self.previous_char = self.chars_iterator.next();
+
+                        Token::Equal
+                    }
+                    Some(other_char) => {
+                        self.previous_char = Some(other_char);
+
+                        Token::Assign
+                    }
+                    None => {
+                        self.previous_char = None;
+
+                        Token::Assign
+                    }
+                }
+            }
+            '!' => {
+                // can be either Bang ("!") or NotEqual ("!=")
+
+                match self.chars_iterator.next() {
+                    Some('=') => {
+                        self.previous_char = self.chars_iterator.next();
+
+                        Token::NotEqual
+                    }
+                    Some(other_char) => {
+                        self.previous_char = Some(other_char);
+
+                        Token::Bang
+                    }
+                    None => {
+                        self.previous_char = None;
+
+                        Token::Bang
+                    }
+                }
+            }
+            illegal => Token::Illegal(Literal(illegal.to_string())),
+        }
+    }
+
+    fn read_token(&mut self, first_char: char) -> Option<Token> {
+        let mut current_char = first_char;
+
+        if current_char.is_whitespace() {
+            if let Some(next_char) = self.get_next_non_whitespace_char_or_eof() {
+                current_char = next_char;
+            } else {
+                return None;
+            }
+        }
+
+        if current_char.is_alphabetic() {
+            Some(self.read_identifier_or_keyword(current_char))
+        } else if current_char.is_ascii_digit() {
+            Some(self.read_integer(current_char))
+        } else {
+            Some(self.read_special_symbol(current_char))
+        }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(current_char) = self.previous_char {
+            self.read_token(current_char)
+        } else {
+            None
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,29 +286,49 @@ mod tests {
 
     #[test]
     fn next_token_from_code_snippet() {
+        // the test input is the same as the one in the book Writing An Interpreter In Go,
+        // except that Monkey code is translated to tempe-lang code
+        // and a few things are changed, to test corner cases (e.g. an identifier containing an underscore)
         let input = "
-        
+
             diketahui five = 5;
-            diketahui ten = 10;
+            diketahui number10 = 10;
+
             diketahui add = fungsi(x, y) {
                 x + y;
             };
 
-            diketahui the_result = add(five, ten);
+            diketahui the_result = add(five, number10);
+            !-/*5;
+            5 < 10 > 5;
+
+            jika (5 < 10) {
+                kembalikan benar;
+            } jika tidak {
+                kembalikan salah;
+            }
+
+            10 == 10;
+            10 != 9;
 
         ";
 
         let expected_tokens = vec![
+            // diketahui five = 5;
             Token::Let,
             Token::Identifier(Literal("five".to_string())),
             Token::Assign,
             Token::Integer(5),
             Token::Semicolon,
+            //
+            // diketahui number10 = 10;
             Token::Let,
-            Token::Identifier(Literal("ten".to_string())),
+            Token::Identifier(Literal("number10".to_string())),
             Token::Assign,
             Token::Integer(10),
             Token::Semicolon,
+            //
+            // diketahui add = fungsi(x, y) {
             Token::Let,
             Token::Identifier(Literal("add".to_string())),
             Token::Assign,
@@ -39,12 +339,18 @@ mod tests {
             Token::Identifier(Literal("y".to_string())),
             Token::ClosingParenthesis,
             Token::OpeningBrace,
+            //
+            // x + y;
             Token::Identifier(Literal("x".to_string())),
             Token::Plus,
             Token::Identifier(Literal("y".to_string())),
             Token::Semicolon,
+            //
+            // };
             Token::ClosingBrace,
             Token::Semicolon,
+            //
+            // diketahui the_result = add(five, number10);
             Token::Let,
             Token::Identifier(Literal("the_result".to_string())),
             Token::Assign,
@@ -52,8 +358,64 @@ mod tests {
             Token::OpeningParenthesis,
             Token::Identifier(Literal("five".to_string())),
             Token::Comma,
-            Token::Identifier(Literal("ten".to_string())),
+            Token::Identifier(Literal("number10".to_string())),
             Token::ClosingParenthesis,
+            Token::Semicolon,
+            //
+            // !-/*5;
+            Token::Bang,
+            Token::Minus,
+            Token::Slash,
+            Token::Asterisk,
+            Token::Integer(5),
+            Token::Semicolon,
+            //
+            // 5 < 10 > 5;
+            Token::Integer(5),
+            Token::LessThan,
+            Token::Integer(10),
+            Token::GreaterThan,
+            Token::Integer(5),
+            Token::Semicolon,
+            //
+            // jika (5 < 10) {
+            Token::If,
+            Token::OpeningParenthesis,
+            Token::Integer(5),
+            Token::LessThan,
+            Token::Integer(10),
+            Token::ClosingParenthesis,
+            Token::OpeningBrace,
+            //
+            // kembalikan true;
+            Token::Return,
+            Token::True,
+            Token::Semicolon,
+            //
+            // } jika tidak {
+            Token::ClosingBrace,
+            Token::If,
+            Token::Not,
+            Token::OpeningBrace,
+            //
+            // kembalikan false;
+            Token::Return,
+            Token::False,
+            Token::Semicolon,
+            //
+            // }
+            Token::ClosingBrace,
+            //
+            // 10 == 10;
+            Token::Integer(10),
+            Token::Equal,
+            Token::Integer(10),
+            Token::Semicolon,
+            //
+            // 10 != 9;
+            Token::Integer(10),
+            Token::NotEqual,
+            Token::Integer(9),
             Token::Semicolon,
         ];
 
@@ -97,86 +459,6 @@ mod tests {
                     panic!("Expected to get a token");
                 }
             }
-        }
-    }
-}
-
-// a Lexer iterates over a text and returns tokens
-// it supports Unicode in identifiers
-pub struct Lexer<'a> {
-    chars_iterator: std::str::Chars<'a>,
-    previous_non_whitespace_char: Option<char>,
-}
-
-impl<'a> Lexer<'a> {
-    /// # Examples
-    ///
-    /// ```
-    /// let source_code = "
-    /// 1 + 1
-    /// ";
-    ///
-    /// let mut lexer = Lexer::new(source_code);
-    /// ```
-    pub fn new(text: &'a str) -> Self {
-        Lexer {
-            chars_iterator: text.chars(),
-            previous_non_whitespace_char: None,
-        }
-    }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = Token;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut token_buffer = if let Some(ch) = self.previous_non_whitespace_char {
-            ch.to_string()
-        } else {
-            String::new()
-        };
-
-        'lexing: while let Some(ch) = self.chars_iterator.next() {
-            // we must guess if the current character signals a new token
-            // in 'a bcd', the space signals a new token (starting with 'b')
-            // in 'ab_42cd(', '(' signals a new token
-            // in '(foobar' or '(42', '(' signals a new token
-            // in '((', there is no new token (but it will be an illegal token)
-
-            if ch.is_whitespace() {
-                if token_buffer.is_empty() {
-                    continue 'lexing;
-                }
-
-                self.previous_non_whitespace_char = None;
-
-                return Some(Token::from(token_buffer.as_str()));
-            }
-
-            let is_new_token_delimiter = match self.previous_non_whitespace_char {
-                None => false,
-                Some(previous_ch) => {
-                    let ch_is_literal = ch.is_alphanumeric() || ch == '_';
-                    let previous_ch_is_literal =
-                        previous_ch.is_alphanumeric() || previous_ch == '_';
-
-                    !(ch_is_literal && previous_ch_is_literal)
-                }
-            };
-
-            self.previous_non_whitespace_char = Some(ch);
-
-            if is_new_token_delimiter && !token_buffer.is_empty() {
-                return Some(Token::from(token_buffer.as_ref()));
-            }
-
-            token_buffer.push(ch);
-        }
-
-        if token_buffer.is_empty() {
-            None
-        } else {
-            Some(Token::from(token_buffer.as_str()))
         }
     }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,58 +1,3 @@
-#[cfg(test)]
-mod tests {
-    use super::{Literal, Token};
-
-    #[test]
-    fn from_str() {
-        let str_and_expected_tokens = vec![
-            ("=", Token::Assign),
-            ("+", Token::Plus),
-            (" +   \n", Token::Plus), // white spaces should be tolerated
-            (",", Token::Comma),
-            (";", Token::Semicolon),
-            ("(", Token::OpeningParenthesis),
-            (")", Token::ClosingParenthesis),
-            ("{", Token::OpeningBrace),
-            ("}", Token::ClosingBrace),
-            ("fungsi", Token::Function),
-            ("diketahui", Token::Let),
-            ("42829", Token::Integer(42829)),
-            ("848.28", Token::Illegal(Literal("848.28".to_string()))), // our language is basic and only supports integers (no floats)
-            (
-                "awesome_variable_42",
-                Token::Identifier(Literal("awesome_variable_42".to_string())),
-            ),
-            (
-                "   awesome_variable_42",
-                Token::Identifier(Literal("awesome_variable_42".to_string())),
-            ), // white spaces shouldn't break tokenization
-            (
-                "1awesome_variable",
-                Token::Illegal(Literal("1awesome_variable".to_string())),
-            ), // identifiers cannot start with a digit
-            (
-                "_awesome_variable",
-                Token::Illegal(Literal("_awesome_variable".to_string())),
-            ), // nor any non-alphabetic character
-            (
-                "สนุกไหม",
-                Token::Identifier(Literal("สนุกไหม".to_string())),
-            ), // it's 21st century, we can support any Unicode letter in our identifiers
-            ("a(", Token::Illegal(Literal("a(".to_string()))),
-        ];
-
-        for (s, expected_token) in str_and_expected_tokens {
-            assert_eq!(Token::from(s), expected_token);
-        }
-    }
-}
-
-// "fungsi" is a direct translation of 'function' in Bahasa Indonesia
-const LITERAL_FUNCTION: &str = "fungsi";
-
-// "diketahui" means something like "It is known that..." in Bahasa Indonesia
-const LITERAL_LET: &str = "diketahui";
-
 #[derive(Debug, Eq, PartialEq)]
 pub struct Literal(pub String);
 
@@ -63,9 +8,28 @@ pub enum Token {
     Identifier(Literal),
     Integer(i64),
 
+    // keywords
+    Function,
+    Let,
+    True,
+    False,
+    If,
+    Not, // not to be confused with Bang ("!"), see keywords module for why this keyword exists
+    Return,
+
+    // operators
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    LessThan,
+    GreaterThan,
+    Equal,
+    NotEqual,
 
+    // other symbols
     Comma,
     Semicolon,
 
@@ -73,58 +37,26 @@ pub enum Token {
     ClosingParenthesis,
     OpeningBrace,
     ClosingBrace,
-
-    Function,
-    Let,
 }
 
-impl From<&str> for Token {
-    fn from(raw_token: &str) -> Self {
-        use self::{Literal, Token};
+pub mod keywords {
 
-        let raw_token = raw_token.trim();
+    // "fungsi" is a direct translation of 'function' in Bahasa Indonesia
+    pub const FUNCTION: &str = "fungsi";
 
-        if raw_token.is_empty() {
-            return Token::Illegal(Literal(String::new()));
-        }
+    // "diketahui" means something like "It is known that..." in Bahasa Indonesia
+    pub const LET: &str = "diketahui";
 
-        match raw_token {
-            "=" => Token::Assign,
-            "+" => Token::Plus,
+    pub const TRUE: &str = "benar";
 
-            "," => Token::Comma,
-            ";" => Token::Semicolon,
+    pub const FALSE: &str = "salah";
 
-            "(" => Token::OpeningParenthesis,
-            ")" => Token::ClosingParenthesis,
-            "{" => Token::OpeningBrace,
-            "}" => Token::ClosingBrace,
+    pub const IF: &str = "jika";
 
-            LITERAL_FUNCTION => Token::Function,
-            LITERAL_LET => Token::Let,
-            _ => {
-                // at this stage, the token is either an identifier or an illegal sequence
-                // identifiers must start with a letter, and may contain only letters, digits and underscores
+    // in Indonesian language, there is no one-word translation for the words "else" or "otherwise"
+    // instead, the best translation for these words is "jika tidak", that is, two words separated by a space
+    // as a result, we must introduce the keyword "tidak", which will be only used for this purpose
+    pub const NOT: &str = "tidak";
 
-                if let Ok(integer) = raw_token.parse::<i64>() {
-                    // since this is only a toy lexer, our implementation doesn't support integers bigger than i64 can hold
-                    // it means that if the source code contains a bigger number, it will fail with Illegal(identifier) and no further explanation
-                    return Token::Integer(integer);
-                }
-
-                let mut chars = raw_token.chars();
-
-                let first_char_is_letter = chars.next().unwrap().is_alphabetic(); // at this stage the string is guaranteed to contain at least one char
-
-                let remaining_chars_are_letters_digits_or_underscore =
-                    chars.all(|ch| ch.is_alphanumeric() || ch == '_');
-
-                if first_char_is_letter && remaining_chars_are_letters_digits_or_underscore {
-                    Token::Identifier(Literal(raw_token.to_string()))
-                } else {
-                    Token::Illegal(Literal(raw_token.to_string()))
-                }
-            }
-        }
-    }
+    pub const RETURN: &str = "kembalikan";
 }
